64131dde956013bc0882f50722b029a475a3e5c1
MarcoHuebner <57489799+MarcoHuebner@users.noreply.github.com>
2021-05-02 13:06:01 +0200

    Cleaned up code, added subdirectories and graphics

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Dissociation Dynamics of H2+ in XUV and IR laser fields
 
+Final project of Computational Quantum Dynamics lecture some years ago. Project and 
+[report](https://dl.orangedox.com/H2DissociationDynamics) made together with @kkiefer.
+
 ## Methods
 
 The following methods should be used:
@@ -34,17 +37,15 @@ Atomic Units:
 
 1. Length in Bohr radii (r_0 = 0.529177 1e-10 m)
 2. Energy given in Hartree energies (E_H = 27.211 eV)
-3. For further (e.g. time) see [here](https://de.wikipedia.org/wiki/Atomare_Einheiten) or [here](https://en.wikipedia.org/wiki/Hartree_atomic_units#Units)
+3. For further (e.g. time) see [here](https://de.wikipedia.org/wiki/Atomare_Einheiten) or
+   [here](https://en.wikipedia.org/wiki/Hartree_atomic_units#Units)
 
 
 ## Steps
 
 ### 1. Find the vibrational eigenstates in the H2+ ground state potential
 
-1. ground state potential = H2p_pot_gerade.dat 
-2. Born Oppenheimer approximation -> get H
-3. exact diagonalization
-4. verify := eigenstates CAN be similar to HO, dependent on how low-lying the eigenstates are and therefore how close the potential is to HO (For HO computations see experiments.ipynb)
+<img src="imgs/bound_eigenstates.gif" width="400"/>
 
 Methods:
 
@@ -54,31 +55,34 @@ Methods:
 
 ### 2. Simulate the wave packet propagation without the IR laser field
 
-(1. compute H(t) using split-step fourier method)
-(2. apply to H2nuclwf.dat)
-
-1. decompose H2nuclwf.dat into vibational eigenstates
-2. compute time-evolution of eigenstates using split-step fourier method
+<img src="imgs/time_evol_wo_laser.gif" width="400"/>
 
 Methods:
 
 * Split-step fourier
-* fourier analysis
+* Fourier analysis
 
 
 ### 3. Simulate the dynamics of the time-dependent system
-Simulate the dynamics of the time-dependent system with two coupled energy surfaces,
-coupled by the time-dependent IR laser field. Visualize the wave packet dynamics.
 
-1. determine time-dependent barrier "height"
-2. compute effect on the time-evolution of the vibrational eigenstates (Do they: Escape/ Escape partially/ Nothing at all?)
+<img src="imgs/laser_field_evolution.gif" width="400"/>
+
+Methods:
+
+* Split-step fourier
+* Fourier analysis
 
 
 ### 4. Scan the delay time and analyze the momentum distribution
-Scan the delay time and analyze the momentum distribution of the dissociation products.
 
-1. See time dependent-escape effects (Where is the connection between escape and break-up?)
-2. ?
+<img src="imgs/titelbild.png" width="400"/>
+
+Methods:
+
+* Exact diagonalization
+* Split-step fourier
+* Fourier analysis
+
 
 ## References
 
--- methods.py
+++ methods.py
@@ -13,15 +13,21 @@ In addition to the different methods a constant for the reduced molecular mass `
 """
 
 import os
+import scipy
+import matplotlib
 import numpy as np
 import scipy.sparse as ss
+import scipy.sparse.linalg as ssla
+
+from typing import Tuple
+from matplotlib import animation
 
 
 # reduced molecule mass ( mu = m1*m2/(m1 + m2) = m_p/2 ) in atomic units (m_p in electron masses)
 mu = 1836/2
 
 
-def load_data(filename, filepath="data"):
+def load_data(filename: str, filepath: str = "data") -> np.ndarray:
     """Load data from a tab separated csv file.
 
     Args:
@@ -34,22 +40,93 @@ def load_data(filename, filepath="data"):
     return np.loadtxt(os.path.join(filepath, filename), delimiter="\t")
 
 
-def hamiltonian(potential, dx):
+def preprocess_data(dip_coupling: np.ndarray, wave_fct: np.ndarray, pot_uneven: np.ndarray,
+                    pot_even: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    """Interpolate loaded .dat files to hard-coded range of 300 with steps of 0.05 atomic units.
+
+        Args:
+            dip_coupling (np.array): Dipole coupling array.
+            wave_fct (np.array): Given initial wave function.
+            pot_uneven (np.array): Photon-excited, anti-binding potential.
+            pot_even (np.array): Ground state potential.
+
+        Returns:
+            (Tuple[np.array*4]) The interpolated arrays.
+    """
+    # global variables
+    unit_limit = 300.0  # should be a multiple of dx, defines total grid size
+    dx = 0.05           # constant for fineness of grid
+
+    # interpolate and add the first missing dipole coupling values
+    interpolation = np.stack((
+        np.linspace(dip_coupling[0, 0], dip_coupling[1, 0], 7, endpoint=True),
+        np.linspace(dip_coupling[0, 1], dip_coupling[1, 1], 7, endpoint=True)
+    ), axis=1)
+
+    dipole_coupling = np.concatenate([interpolation[1:-1], dip_coupling[1:]])
+
+    # interpolate between values at 10 and 1000 Bohr radii for larger grids
+    if unit_limit > 10.0:
+        units = np.linspace(10.0 + dx, unit_limit, int((unit_limit - 10.0) / dx), endpoint=True)
+        interpolation = np.stack((units, 0.494112 * units), axis=1)
+        dip_coupling = np.concatenate([dipole_coupling, interpolation])
+
+    # interpolate potentials and wave function for larger grids
+    if unit_limit > 30.0:
+        units = np.linspace(30.0 + dx, unit_limit, int((unit_limit - 30.0) / dx), endpoint=True)
+
+        wave_fct = np.concatenate([
+            wave_fct[wave_fct[:, 0] <= 30.0],
+            np.stack((units, np.zeros(len(units))), axis=1)
+        ])
+
+        pot_uneven = np.concatenate([
+            pot_uneven[pot_uneven[:, 0] <= 30.0],
+            np.stack((units, np.zeros(len(units))), axis=1)
+        ])
+
+        pot_even = np.concatenate([
+            pot_even[pot_even[:, 0] <= 30.0],
+            np.stack((units, -6.66667e-06 * np.ones(len(units))), axis=1)
+        ])
+
+    # limit to desired range, ensure same grid (discard values at 0.0 due to diverging potential)
+    pot_uneven = pot_uneven[pot_uneven[:, 0] <= unit_limit]
+    pot_even = pot_even[pot_even[:, 0] <= unit_limit]
+    wave_function = wave_fct[wave_fct[:, 0] <= unit_limit][1:]
+    dipole_coupling = dip_coupling[dip_coupling[:, 0] <= unit_limit]
+
+    return dipole_coupling, wave_function, pot_uneven, pot_even
+
+
+def hamiltonian(potential: np.ndarray, dx: float) -> scipy.sparse.csr_matrix:
     """Compute the hamiltonian for the given potential.
 
     Args:
         potential (np.array): The potential as vector containing the diagonal.
         dx (float): step size of hamiltonian.
     Returns:
-        (scipy.spare.csr_matrix): The hamiltonian as a sparse matrix.
+        (scipy.sparse.csr_matrix): The hamiltonian as a sparse matrix.
     """
-    size = len(potential)
-
-    # diagonals of kinetic energy matrix
-    main_diag = (1.0 / (dx**2 * mu)) * np.ones(size) + potential
-    off_diag = (-0.5 / (dx**2 * mu)) * np.ones(size-1)
+    # diagonals of kinetic energy matrix (second order diff. quot.) + potential
+    main_diag = (1.0 / (dx**2 * mu)) * np.ones(len(potential)) + potential
+    off_diag = (-0.5 / (dx**2 * mu)) * np.ones(len(potential)-1)
 
     return ss.diags([off_diag, main_diag, off_diag], [-1, 0, 1])
+
+
+def save_anim(anim: matplotlib.animation.FuncAnimation, filename: str, fps: int = 1) -> None:
+    """Load data from a tab separated csv file.
+
+        Args:
+            anim (matplotlib.animation.FuncAnimation): Animation figure.
+            filename (str): The name of the file, should be a .gif.
+            fps (int): Frames to show per second in .gif.
+    """
+    writer = animation.PillowWriter(fps=fps)
+    anim.save('plots/' + filename, writer=writer, dpi=300)
+
+    return None
 
 
 class TimeEvolution:
@@ -59,10 +136,14 @@ class TimeEvolution:
         initial_state (np.array): The initial state.
         hamiltonian (np.array): The Hamiltonian of the system.
     """
-    def __init__(self, initial_state, hamiltonian):
-        # TODO: add some assertions to check dims
-        eigenvals, eigenvecs = np.linalg.eigh(hamiltonian)
+    def __init__(self, initial_state: np.ndarray, hamiltonian: np.ndarray) -> None:
+        assert initial_state.shape[0] == hamiltonian.shape[0], "Make sure initial state dim is compatible with " \
+                                                               "hamiltonian dim for matrix multiplication"
 
+        # compute eigenvals (-energies) and eigenvecs (-states)
+        eigenvals, eigenvecs = ssla.eigsh(hamiltonian, k=hamiltonian.shape[0])
+
+        # select only bound states
         mask = eigenvals <= 0.0
         
         self.initial_state = np.conjugate(eigenvecs[:, mask].T) @ initial_state
@@ -70,7 +151,7 @@ class TimeEvolution:
         self.eigenvals = eigenvals[mask]
         self.eigenvecs = eigenvecs[:, mask]
 
-    def __call__(self, time):
+    def __call__(self, time: float) -> np.ndarray:
         """Compute the state at the given time.
 
         Args:
@@ -95,14 +176,14 @@ class SplitStepMethod:
         step_size (float): The step size in the frequency domain.
         potential (np.array): The potential energy of the system (optional).
     """
-    def __init__(self, num_points, step_size, potential=None):
+    def __init__(self, num_points: int, step_size: float, potential: np.ndarray = None) -> None:
         self.kvalues_squared = (2 * np.pi * np.fft.fftfreq(num_points, d=step_size))**2
         
         self.num_points = num_points
         self.step_size = step_size
         self.potential = potential
 
-    def __call__(self, state, dt, potential=None):
+    def __call__(self, state: np.ndarray, dt: float, potential: np.ndarray = None) -> np.ndarray:
         """Compute a single step with the split-step method.
 
         Args:
@@ -133,11 +214,10 @@ class LaserFieldPotential:
         pot_even (np.array): the binding part of the constant potential
         pot_uneven (np.array): the repellent part of the constant potential
         dipole_coupling (np.array): dipole coupling between states
-        dx (float): step size of potential
         tau (float): time delay of one oscillation period
     """
-    def __init__(self, pot_even, pot_uneven, dipole_coupling, dx, tau=1075.3):
-        # TODO: add more documentation and maybe dx
+    def __init__(self, pot_even: np.ndarray, pot_uneven: np.ndarray, dipole_coupling: np.ndarray,
+                 tau: float = 1075.3) -> None:
         # provide constants taken from reference papers (in atomic units)
         self.tau = tau
         self.t_fwhm = 355.7
@@ -145,10 +225,11 @@ class LaserFieldPotential:
         self.f_0 = 0.01205
 
         self.dipole_coupling = dipole_coupling[:, 1]
+        # define intermediate results for shorter code
         self.pot_sum = pot_even[:, 1] + (pot_uneven[:, 1] - self.omega)
         self.pot_diff = pot_even[:, 1] - (pot_uneven[:, 1] - self.omega)
 
-    def __call__(self, t):
+    def __call__(self, t: float) -> np.ndarray:
         """Compute V(t).
 
         Args:
@@ -157,10 +238,13 @@ class LaserFieldPotential:
         Returns:
             (np.array) The potential as a vector
         """
-        # TODO: add more documentation
+        # compute eq. (11) of linked project report in smaller steps
         g_t = np.exp(-(2*np.sqrt(np.log(2)) * (t - self.tau) / self.t_fwhm)**2) 
         f_t = self.f_0 * g_t * np.cos(self.omega * (t - self.tau))
+        # assemble above's steps to W(t)
         pot_laser = -f_t * self.dipole_coupling
 
+        # calculate final steps for solved eigenvalue eq. (7) of coupled energy surfaces
         potential = np.sqrt(self.pot_diff**2 + 4 * np.real(pot_laser**2))
-        return 0.5 * self.pot_sum - 0.5 * potential
+
+        return 0.5 * (self.pot_sum - potential)
